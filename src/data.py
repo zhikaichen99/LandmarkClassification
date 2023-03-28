81b1022f1cd04fec3a1aafa2fedc8411e9141b44
zhikaichen99 <zhikaichen1999@gmail.com>
2023-03-28 12:15:37 -0400

    added comments to data.py

--- src/data.py
+++ src/data.py
@@ -15,13 +15,13 @@ def get_data_loaders(
     """
     Create and returns the train_one_epoch, validation and test data loaders.
 
-    :param batch_size: size of the mini-batches
-    :param valid_size: fraction of the dataset to use for validation. For example 0.2
-                       means that 20% of the dataset will be used for validation
-    :param num_workers: number of workers to use in the data loaders. Use -1 to mean
-                        "use all my cores"
-    :param limit: maximum number of data points to consider
-    :return a dictionary with 3 keys: 'train_one_epoch', 'valid' and 'test' containing respectively the
+    Inputs:
+        batch_size (int) - size of the mini-batches
+        valid_size (float) - fraction of the dataset to use for validation. 
+        num_workers (int) - number of workers to use in the data loaders. Use -1 to mean "use all my cores"
+        limit (int) - maximum number of data points to consider
+    Outputs:
+        dictionary with 3 keys: 'train_one_epoch', 'valid' and 'test' containing respectively the
             train_one_epoch, validation and test data loaders
     """
 
@@ -29,7 +29,6 @@ def get_data_loaders(
         # Use all cores
         num_workers = multiprocessing.cpu_count()
 
-    # We will fill this up later
     data_loaders = {"train": None, "valid": None, "test": None}
 
     base_path = Path(get_data_location())
@@ -74,12 +73,9 @@ def get_data_loaders(
     # Create train and validation datasets
     train_data = datasets.ImageFolder(
         base_path / "train",
-        # YOUR CODE HERE: add the appropriate transform that you defined in
         # the data_transforms dictionary
         transform = data_transforms["train"]
     )
-    # The validation dataset is a split from the train_one_epoch dataset, so we read
-    # from the same folder, but we apply the transforms for validation
     valid_data = datasets.ImageFolder(
         base_path / "train",
         # YOUR CODE HERE: add the appropriate transform that you defined in
@@ -111,14 +107,13 @@ def get_data_loaders(
         num_workers=num_workers,
     )
     data_loaders["valid"] = torch.utils.data.DataLoader(
-        # YOUR CODE HERE
         valid_data,
         batch_size = batch_size,
         sampler = valid_sampler,
         num_workers = num_workers,
     )
 
-    # Now create the test data loader
+    # create the test data loader
     test_data = datasets.ImageFolder(
         base_path / "test",
         transform = data_transforms['test']
@@ -144,18 +139,17 @@ def visualize_one_batch(data_loaders, max_n: int = 5):
     """
     Visualize one batch of data.
 
-    :param data_loaders: dictionary containing data loaders
-    :param max_n: maximum number of images to show
-    :return: None
+    Inputs:
+        data_loaders - dictionary containing data loaders
+        max_n (int) - maximum number of images to show
+    Output:
+        None
     """
 
-    # YOUR CODE HERE:
-    # obtain one batch of training images
-    # First obtain an iterator from the train dataloader
-    dataiter  = iter(data_loaders['train'])# YOUR CODE HERE
-    # Then call the .next() method on the iterator you just
-    # obtained
-    images, labels  = dataiter.next() # YOUR CODE HERE
+
+    dataiter  = iter(data_loaders['train'])
+
+    images, labels  = dataiter.next()
 
     # Undo the normalization (for visualization purposes)
     mean, std = compute_mean_and_std()
@@ -168,12 +162,10 @@ def visualize_one_batch(data_loaders, max_n: int = 5):
 
     images = invTrans(images)
 
-    # YOUR CODE HERE:
     # Get class names from the train data loader
     class_names  = data_loaders['train'].dataset.classes# YOUR CODE HERE
 
-    # Convert from BGR (the format used by pytorch) to
-    # RGB (the format expected by matplotlib)
+    # Convert from BGR (the format used by pytorch) to RGB (the format expected by matplotlib)
     images = torch.permute(images, (0, 2, 3, 1)).clip(0, 1)
 
     # plot the images in the batch, along with the corresponding labels
@@ -181,48 +173,10 @@ def visualize_one_batch(data_loaders, max_n: int = 5):
     for idx in range(max_n):
         ax = fig.add_subplot(1, max_n, idx + 1, xticks=[], yticks=[])
         ax.imshow(images[idx])
-        # print out the correct label for each image
-        # .item() gets the value contained in a Tensor
+        # print out the correct label for each image .item() gets the value contained in a Tensor
         ax.set_title(class_names[labels[idx].item()])
 
 
-######################################################################################
-#                                     TESTS
-######################################################################################
-import pytest
 
 
-@pytest.fixture(scope="session")
-def data_loaders():
-    return get_data_loaders(batch_size=2, num_workers=0)
 
-
-def test_data_loaders_keys(data_loaders):
-
-    assert set(data_loaders.keys()) == {"train", "valid", "test"}, "The keys of the data_loaders dictionary should be train, valid and test"
-
-
-def test_data_loaders_output_type(data_loaders):
-    # Test the data loaders
-    dataiter = iter(data_loaders["train"])
-    images, labels = dataiter.next()
-
-    assert isinstance(images, torch.Tensor), "images should be a Tensor"
-    assert isinstance(labels, torch.Tensor), "labels should be a Tensor"
-    assert images[0].shape[-1] == 224, "The tensors returned by your dataloaders should be 224x224. Did you " \
-                                       "forget to resize and/or crop?"
-
-
-def test_data_loaders_output_shape(data_loaders):
-    dataiter = iter(data_loaders["train"])
-    images, labels = dataiter.next()
-
-    assert len(images) == 2, f"Expected a batch of size 2, got size {len(images)}"
-    assert (
-        len(labels) == 2
-    ), f"Expected a labels tensor of size 2, got size {len(labels)}"
-
-
-def test_visualize_one_batch(data_loaders):
-
-    visualize_one_batch(data_loaders, max_n=2)
